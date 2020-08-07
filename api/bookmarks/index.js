62a8c36b5ed42b0b0eb093908927fd35058fcf23
Kevin Atkins <atkinsk021@gmail.com>
2020-08-07 13:41:44 +0100

    update and delete for api

--- api/bookmarks/index.js
+++ api/bookmarks/index.js
@@ -3,10 +3,12 @@ import {bookmarks} from './bookmarks';
 
 const router = express.Router();
 
+//GET REQUEST
 router.get('/', (req, res) => {
   res.send({ bookmarks: bookmarks });
 });
 
+//POST REQUEST
 router.post('/', (req, res) => {
         let newBookmark = req.body;
         if (newBookmark){
@@ -17,4 +19,32 @@ router.post('/', (req, res) => {
       }
 });
 
+//UPDATE REQUEST
+router.put('/:id', (req, res) => {
+  const key = req.params.id;
+  const updateBookmark = req.body;
+  const index = bookmarks.map((bookmark)=>{
+return bookmark.link;
+}).indexOf(key);
+         if (index !== -1) {
+            bookmarks.splice(index, 1, {title: updateBookmark.title, link: updateBookmark.link});
+            res.status(200).send({message: 'Bookmark successfully updated'});
+           } else {
+       res.status(400).send({message: 'Unable to find Bookmark in request. No update has taken place'});
+   }
+});
+
+//DELETE REQUEST
+router.delete('/:id', (req, res) => {
+  const key = req.params.id;
+  const index = bookmarks.map((bookmark)=>{
+return bookmark.link;
+}).indexOf(key);
+ if (index > -1) {
+bookmarks.splice(index, 1);
+     res.status(200).send({message: `Deleted bookmark : ${key}.`});
+ } else {
+   res.status(400).send({message: `Unable to find bookmark of link : ${key}.`});
+   }
+});
 export default router;
