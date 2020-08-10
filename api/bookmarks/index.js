9067c256185fbde45e97bcd6ff2c0b0f31cbbeb0
Kevin Atkins <atkinsk021@gmail.com>
2020-08-10 22:03:15 +0100

    attempted to connect webapp1

--- api/bookmarks/index.js
+++ api/bookmarks/index.js
@@ -1,23 +1,50 @@
 import express from 'express';
-import {bookmarks} from './bookmarks';
+//import {bookmarks} from './bookmarks';
+import stubAPI from './stubAPI';
 
 const router = express.Router();
 
 //GET REQUEST
 router.get('/', (req, res) => {
+  const bookmarks = stubAPI.getAll();
   res.send({ bookmarks: bookmarks });
 });
 
 //POST REQUEST
 router.post('/', (req, res) => {
         let newBookmark = req.body;
-        if (newBookmark){
-          bookmarks.push({title: newBookmark.title, link : newBookmark.link}) ;
-          res.status(201).send({message: "Bookmark Created"});
-      }else{
-            res.status(400).send({message: "Unable to create Bookmark in request. No Bookmark Found in body"});
-      }
+        if (newBookmark && stubAPI.add(newBookmark.title, newBookmark.link)) {
+          return res.status(201).send({message: 'Bookmarks Created'});
+     }
+     return res.status(400).send({message: 'Unable to find Bookmark in request.'});
 });
+
+// get a post
+router.get('/:id', (req, res) => {
+  const id = req.params.id;
+  const bookmark = stubAPI.getBookmark(id);
+
+     if (post) {
+             return res.status(200).send(bookmark);
+            }
+            return res.status(404).send({message: `Unable to find Bookmark ${id}`});
+});
+
+// upvote a post
+router.post('/:id/visit', (req, res) => {
+  const id = req.params.id;
+         if (stubAPI.visit(id)) {
+              return res.status(200).send({message: `Bookmark ${id} visited`});
+         }
+         return res.status(404).send({message: `Unable to find Bookmark ${id}`});
+});
+
+//if (newBookmark){
+ // bookmarks.push({title: newBookmark.title, link : newBookmark.link}) ;
+ // res.status(201).send({message: "Bookmark Created"});
+//}else{
+ //   res.status(400).send({message: "Unable to create Bookmark in request. No Bookmark Found in body"});
+//}
 
 //UPDATE REQUEST
 router.put('/:id', (req, res) => {
@@ -47,4 +74,5 @@ bookmarks.splice(index, 1);
    res.status(400).send({message: `Unable to find bookmark of link : ${key}.`});
    }
 });
+
 export default router;
